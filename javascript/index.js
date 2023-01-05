d60a5d6091f9255745cad8b4bae7f4a06884f19d
Vinicius Lima <viniciuslimavirtuel@gmail.com>
2023-01-05 12:21:33 -0300

    update system with localStorage

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,16 @@
 
 const Main = {
 
-  init: function () {
+    tasks: [],
+
+    init: function () {
     this.cacheSelectors();
     this.bindEvents();
+    this.getStoraged();
+    this.buildTasks();
   },
 
-  cacheSelectors: function () {
+    cacheSelectors: function () {
     // uso do $ para indicar as variáveis que armazenam algum elemento HTML 
 
     this.$checkButtons = document.querySelectorAll('.check');
@@ -15,7 +19,7 @@ const Main = {
     this.$removeButtons = document.querySelectorAll('.remove');
   },
 
-  bindEvents: function () {
+    bindEvents: function () {
     const self = this // o this se refere ao obj Main
 
     this.$checkButtons.forEach(function (button) {
@@ -29,7 +33,36 @@ const Main = {
     });
   },
 
-  Events: {
+    getStoraged: function () {
+    let tasks = localStorage.getItem('tasks');
+
+   
+    
+      this.tasks = JSON.parse(tasks);
+    
+  },
+
+    buildTasks: function () {
+      let html = '';
+      const self = this
+
+      self.tasks.forEach(item => {
+        html += `
+          <li>
+            <div class="check"></div>
+            <label class="task">
+              ${item.task}
+            </label>
+            <button class="remove"></button>
+          </li>
+        
+        `
+      });
+
+      this.$list.innerHTML = html;
+  },
+
+    Events: {
     checkButton_click: function (e) {
       let li = e.target.parentElement
       let isDone = li.classList.contains('done')
@@ -42,13 +75,25 @@ const Main = {
 
     },
 
+    removeButtons_click: function (e) {
+      let li = e.target.parentElement
+      
+      li.classList.add('removed')
+        
+      setTimeout(function () {
+
+        li.classList.add('hidden')
+      }, 300)
+    },
+
+
     inputTask_keypress: function (e) {
       //Dentro de uma função de evento - seja de click ou de onkeypress, onmousemove - o this vai ser o próprio elemento que o evento foi adicionado. Usa-se o método bind()
 
       let key = e.key
       let value = e.target.value
       
-      if(key === 'Enter'){
+      if(key === 'Enter') {
         this.$list.innerHTML += `
           <li>
             <div class="check"></div>
@@ -63,17 +108,27 @@ const Main = {
 
         this.cacheSelectors();
         this.bindEvents();
-      }
+
+        const getTasks = localStorage.getItem("tasks")
+
+
+        if (!getTasks){
+          const obj = [{task: value}];
+          return localStorage.setItem("tasks", JSON.stringify(obj));
+          
+        }
+
+        if (getTasks){
+          const obj = [ {task: value}, ...JSON.parse(getTasks) ];
+          return localStorage.setItem("tasks", JSON.stringify(obj))
+        };
+        
+        
+      };
+      
   },
 
-    removeButtons_click: function (e) {
-      let li = e.target.parentElement
-      li.classList.add('removed')
 
-      setTimeout(function () {
-        li.classList.add('hidden')
-      }, 300)
-    }
 }
 
 
